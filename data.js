5a7f79d065cf49c54b1b4ec7b2096a078fd41533
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-04-26 18:15:17 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,4 @@
+module.exports = {
+  token: '2059835486:AAF2CHMnbboAyL-wwOGqonn_GAkHLFSvMG8', // string, token of bot from @BotFather
+  chatId: '@innoadsstage' // number or string with id of chat, where bot should send notifications
+}
