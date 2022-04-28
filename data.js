6437819efbdbee5ea6cb53651db005ceb2aa4e06
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-04-28 11:44:21 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const StageToken = '2039257826:AAFSaT-Ox18uMyNWBLib02Jl3Nn3r_7_Sjo'
 const ProdToken = '2059835486:AAF2CHMnbboAyL-wwOGqonn_GAkHLFSvMG8'
 module.exports = {
-    token: StageToken,
+    token: ProdToken,
     chatId: '@innoadsstage' // number or string with id of chat, where bot should send notifications
 }
