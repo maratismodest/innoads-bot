72fc72cb2866bb12843dd4f916da9fb75ba5fe87
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-05-03 15:52:52 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,9 @@
 // @InnoAdsPostBot
 const ProdToken = '5142455295:AAEVfMbX6K03WGB7fse_Mpqr8V80uQQA1IE'
+// @InnoAdsStageBot
+const StageToken = '5322899789:AAFXwBt5zjbfUU6J9KGVKKBNr-4bsUkeNkQ'
+// @InnoAdsGitBot
+const GitToken = '5313737775:AAGQz6DY-7voZKd17s-PM6HZzGUhJHmuoIc'
 
 const testChat = '@innoadsstage'
 const prodChat = '@innoads'
