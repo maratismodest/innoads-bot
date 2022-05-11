a2812943e669d319b67bc021a2bc91bdf09ef8e2
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-05-11 21:29:37 +0300

    commit

--- uitls/constants.js
+++ uitls/constants.js
@@ -3,6 +3,7 @@ const options = [
     {value: 2, label: "Куплю"},
     {value: 3, label: "Услуги"},
     {value: 4, label: "Вакансии"},
+    {value: 5, label: "Недвижимость"},
 ];
 
 module.exports = {
