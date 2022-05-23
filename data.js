3613661cf82406e917709fe748d8472c237fb154
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-05-23 20:18:37 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const BOT_BACKEND = 'https://innoads-backend.herokuapp.com'
+
+// InnoAdsHelpBot
+// const BOT_TOKEN = '5142455295:AAEVfMbX6K03WGB7fse_Mpqr8V80uQQA1IE'
+// const BOT_CHAT = '@innoads'
+
+// InnoAdsGitBot
+const BOT_TOKEN = '5313737775:AAGQz6DY-7voZKd17s-PM6HZzGUhJHmuoIc'
+const BOT_CHAT = '@innoadsstage'
+
+const data = {
+    BOT_BACKEND,
+    BOT_TOKEN,
+    BOT_CHAT
+}
+
+module.exports = data
