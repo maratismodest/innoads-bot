ba81a4e6c20a1fe7c4b7afa7bd53fb618802aa68
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-06-08 15:15:12 +0300

    promoted

--- uitls/functions.js
+++ uitls/functions.js
@@ -36,6 +36,11 @@ const postUser = async (req) => {
     }
 }
 
+const randomInteger = (min, max) => {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
 module.exports = {
-    getLink, postUser
+    getLink, postUser, randomInteger
 }
